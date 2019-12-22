59159d5bf07e5de0f174302fb3dc904258d24d84
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-12-22 17:35:33 +0100

    feat(main): add API handler prefix and countries endpoint

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"encoding/json"
 )
+
+const apiPathPrefix = "/api/v1"
 
 var (
 	port = ":8080"
@@ -14,10 +17,19 @@ var (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", indexHandler).Methods("GET")
+
+	api := router.PathPrefix(apiPathPrefix).Subrouter()
+	api.HandleFunc("/countries", countriesHandler).Methods("GET")
+	
 	log.Println("CashCalc 2020 is up and running on port", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to CashCalc 2020!")
+}
+
+func countriesHandler(w http.ResponseWriter, r *http.Request) {
+	countries := getAirCountriesFromJSON()
+	json.NewEncoder(w).Encode(countries)
 }

ec1a0218e6b8985ec9bb2908c2c76615e3d04763
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-12-31 11:36:27 +0100

    refactor(main): delete /api/v1 prefix and unnecessary functions from main pkg

--- main.go
+++ main.go
@@ -4,28 +4,16 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
-	"encoding/json"
 )
 
-const apiPathPrefix = "/api/v1"
-
-var (
-	port = ":8080"
-)
+var port = ":8080"
 
 func main() {
 	router := mux.NewRouter()
 
 	http.HandleFunc("/favicon.ico", faviconHandler)
 	
-	api := router.PathPrefix(apiPathPrefix).Subrouter()
-	api.HandleFunc("/countries", countriesHandler).Methods("GET")
-
-	buildHandler := http.FileServer(http.Dir("frontend/"))
-	router.PathPrefix("/").Handler(buildHandler)
-
-	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("frontend/static")))
-	router.PathPrefix("/static/").Handler(staticHandler)
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("frontend/")))
 	
 	log.Println("CashCalc 2020 is up and running on port", port)
 	log.Fatal(http.ListenAndServe(port, router))
@@ -33,13 +21,4 @@ func main() {
 
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "frontend/favicon.ico")
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-		
-}
-
-func countriesHandler(w http.ResponseWriter, r *http.Request) {
-	countries := getAirCountriesFromJSON()
-	json.NewEncoder(w).Encode(countries)
 }

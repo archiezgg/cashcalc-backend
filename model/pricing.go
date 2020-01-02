35eb7bf4e3b06babba02e8f75d834a5acbe1ec32
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 23:10:12 +0100

    feat(pricing): add func to get air pricings from DB

--- model/pricing.go
+++ model/pricing.go
@@ -1,7 +1,13 @@
 package model
 
 import (
+	"context"
+	"log"
 	"os"
+
+	"github.com/IstvanN/cashcalc-backend/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -12,4 +18,25 @@ var (
 type Pricing struct {
 	ZoneNumber      int
 	Fares, DocFares []int
+}
+
+// GetAirPricingsFromDB returns with a slice of all elements of the air pricings collection
+func GetAirPricingsFromDB() (airPricings []Pricing) {
+	coll := database.GetCollectionByName(airPricingsCollectionName)
+	cur, err := coll.Find(context.TODO(), bson.D{{}}, options.Find())
+	if err != nil {
+		log.Printf("retrieving collection %v failed: %v\n", airCountriesCollectionName, err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var p Pricing
+		err := cur.Decode(&p)
+		if err != nil {
+			log.Println("error while retrieving air pricings, ", err)
+		} else {
+			airPricings = append(airPricings, p)
+		}
+	}
+	cur.Close(context.TODO())
+	return
 }

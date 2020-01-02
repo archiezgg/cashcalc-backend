180c4af0b098e2c9cb88106e814f1ff0bd63cdff
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 15:32:26 +0100

    fix(main): fix calling of controller and db startup funcs

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"github.com/IstvanN/cashcalc-backend/controller"
-	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+
+	"github.com/IstvanN/cashcalc-backend/controller"
+	"github.com/IstvanN/cashcalc-backend/database"
 )
 
 var port = ":8080"
 
 func main() {
-	controller.Startup()
+	router := controller.StartupRouter()
+	database.Startup()
 
 	log.Println("CashCalc 2020 is up and running on port", port)
 	log.Fatal(http.ListenAndServe(port, router))

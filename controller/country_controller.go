c2c3b9e8e628199c6a2c8d503091116854d1fb50
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 22:34:13 +0100

    feat(countriesHandler): add endpoint to handle countries based on the param (road or air)

--- controller/country_controller.go
+++ controller/country_controller.go
@@ -2,17 +2,27 @@ package controller
 
 import (
 	"encoding/json"
-	"github.com/IstvanN/cashcalc-backend/model"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+
+	"github.com/IstvanN/cashcalc-backend/model"
+	"github.com/gorilla/mux"
 )
 
-func registerCountriesRoutes(router *httprouter.Router) {
-	router.GET("/countries", allCountriesHandler)
+func registerCountriesRoutes(router *mux.Router) {
+	router.HandleFunc("/countries", allCountriesHandler).Methods("GET").Queries("type", "{type:[a-zA-Z]+}")
 }
 
-func allCountriesHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	countries := model.GetAirCountriesFromDB()
-	setContentTypeToJSON(w)
-	json.NewEncoder(w).Encode(countries)
+func allCountriesHandler(w http.ResponseWriter, r *http.Request) {
+	switch t := mux.Vars(r)["type"]; t {
+	case "air":
+		setContentTypeToJSON(w)
+		countries := model.GetAirCountriesFromDB()
+		json.NewEncoder(w).Encode(countries)
+	case "road":
+		setContentTypeToJSON(w)
+		countries := model.GetRoadCountriesFromDB()
+		json.NewEncoder(w).Encode(countries)
+	default:
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
 }

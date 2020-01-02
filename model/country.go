908f9ae0574d5514b059a700270a358f4d927a4a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 20:40:41 +0100

    feat(getRoadCountries): add func to get the road countries from DB

--- model/country.go
+++ model/country.go
@@ -10,7 +10,8 @@ import (
 )
 
 const (
-	airCountriesCollectionName = "countries_air"
+	airCountriesCollectionName  = "countries_air"
+	roadCountriesCollectionName = "countries_road"
 )
 
 // Country stores the countries with name and a zone number
@@ -34,6 +35,27 @@ func GetAirCountriesFromDB() (airCountries []Country) {
 			log.Println("error while retrieving air countries, ", err)
 		} else {
 			airCountries = append(airCountries, c)
+		}
+	}
+	cur.Close(context.TODO())
+	return
+}
+
+// GetRoadCountriesFromDB returns with an array of all the elements of the roadCountries collection
+func GetRoadCountriesFromDB() (roadCountries []Country) {
+	coll := database.GetCollectionByName(roadCountriesCollectionName)
+	cur, err := coll.Find(context.TODO(), bson.D{{}}, options.Find())
+	if err != nil {
+		log.Printf("retrieving collection %v failed: %v\n", airCountriesCollectionName, err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var c Country
+		err := cur.Decode(&c)
+		if err != nil {
+			log.Println("error while retrieving air countries, ", err)
+		} else {
+			roadCountries = append(roadCountries, c)
 		}
 	}
 	cur.Close(context.TODO())

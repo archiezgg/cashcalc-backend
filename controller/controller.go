9fdfb7d30fb3332462a88fe757d832f6eaa5909c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 15:38:11 +0100

    feat(welcome): add endpoint to / to welcome client

--- controller/controller.go
+++ controller/controller.go
@@ -9,12 +9,17 @@ import (
 func StartupRouter() (router *httprouter.Router) {
 	router = httprouter.New()
 	router.GET("/favicon.ico", faviconHandler)
+	router.GET("/", welcomeHandler)
 	registerCountriesRoutes(router)
 	return
 }
 
 func faviconHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.ServeFile(w, r, "frontend/favicon.ico")
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Write([]byte("Welcome to CashCalc 2020!"))
 }
 
 func setContentTypeToJSON(w http.ResponseWriter) {

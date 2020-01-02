62fd4700505ae27a6cca1b12721f14e6c6313123
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-02 16:16:34 +0100

    feat(db): add func to get the collection by name and make the mongoClient available package-wide

--- database/database.go
+++ database/database.go
@@ -15,19 +15,28 @@ var (
 	mongoPassword = os.Getenv("MONGO_PW")
 	mongoHostURL  = os.Getenv("MONGO_HOST")
 	mongoDBName   = os.Getenv("MONG_DB_NAME")
+
+	mongoClient *mongo.Client
 )
 
 // Startup is the init call of the mongo DB, supposed to be called in the main function
-func Startup() {
+func Startup() *mongo.Client {
 	dbSpec := fmt.Sprintf("mongodb+srv://%v:%v@%v", mongoUser, mongoPassword, mongoHostURL)
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbSpec))
+	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbSpec))
 	if err != nil {
 		log.Fatal("Couldn't connect to database: ", err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	err = mongoClient.Ping(context.TODO(), nil)
 	if err != nil {
 		log.Fatal("Database is not responding!")
 	}
 	log.Println(("Successfully connected to the database!"))
+	return mongoClient
+}
+
+// GetCollectionByName returns a collection type from the db by its name
+func GetCollectionByName(collectionName string) *mongo.Collection {
+	coll := mongoClient.Database(mongoDBName).Collection(collectionName)
+	return coll
 }

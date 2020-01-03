1d449c2e086b5f2f08c8d89d83521d0f606e4224
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-01-03 01:50:26 +0100

    feat(pricing_controller): add handler to return pricing fares

--- controller/pricing_controller.go
+++ controller/pricing_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/IstvanN/cashcalc-backend/model"
 	"github.com/gorilla/mux"
@@ -11,6 +12,7 @@ import (
 
 func registerPricingsRoutes(router *mux.Router) {
 	router.HandleFunc("/pricings", allPricingsHandler).Methods("GET").Queries("type", "{type:[a-zA-Z]+}")
+	router.HandleFunc("/pricings/{zn:[0-9]}", pricingFaresByZoneNumberHandler).Methods("GET").Queries("type", "{type:[a-zA-Z]+}")
 }
 
 func allPricingsHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +30,28 @@ func allPricingsHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 		}
 		json.NewEncoder(w).Encode(roadPricings)
+	default:
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	}
+}
+
+func pricingFaresByZoneNumberHandler(w http.ResponseWriter, r *http.Request) {
+	setContentTypeToJSON(w)
+
+	zn, _ := strconv.Atoi(mux.Vars(r)["zn"])
+	switch t := mux.Vars(r)["type"]; t {
+	case "air":
+		airFares, err := model.GetAirPricingFaresByZoneNumber(zn)
+		if err != nil {
+			log.Println(err)
+		}
+		json.NewEncoder(w).Encode(airFares)
+	case "road":
+		roadFares, err := model.GetRoadPricingFaresByZoneNumber(zn)
+		if err != nil {
+			log.Println(err)
+		}
+		json.NewEncoder(w).Encode(roadFares)
 	default:
 		http.Error(w, http.StatusText(400), http.StatusBadRequest)
 	}

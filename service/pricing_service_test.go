856dd43c22e626f8b74aad0a4612a942cfb232c6
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-02-07 16:08:51 +0100

    fix(test): add more cases to validateZoneNumber test

--- service/pricing_service_test.go
+++ service/pricing_service_test.go
@@ -1,26 +1,29 @@
 package service
 
-import "testing"
-
-import "fmt"
-
-import "reflect"
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
 
 func TestValidateZoneNumber(t *testing.T) {
-	errorMsg := "the zone number %v is invalid"
+	e := errors.New("")
 	tables := []struct {
 		x   int
 		err error
 	}{
 		{0, nil},
 		{9, nil},
-		{10, fmt.Errorf(errorMsg, 10)},
+		{3, nil},
+		{5, nil},
+		{-1, e},
+		{10, e},
 	}
 
 	for _, table := range tables {
 		err := validateZoneNumber(table.x)
-		if reflect.TypeOf(table.x) != reflect.TypeOf(table.err) {
-			t.Errorf("validateZoneNumber(%v) failed: expected: %T, got: %T", table.x, table.err, err)
+		if reflect.TypeOf(err) != reflect.TypeOf(table.err) {
+			t.Errorf("validateZoneNumber(%v) failed: expected type: %T, got: %T", table.x, table.err, err)
 		}
 	}
 }

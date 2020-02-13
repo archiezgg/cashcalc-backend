162d4161b42b068e2273d26293372c24d203659e
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-02-13 22:17:32 +0100

    feat(pricing_service): add function that increases a number with given vat + tests

--- services/pricing_service.go
+++ services/pricing_service.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 )
 
-// func getVatAsMultiplier() float64 {
-// 	return 1 + ()
-// }
+// IncreaseWithVat takes a float64 and a percentage as parameter
+// and returns with the vat-increased result
+func IncreaseWithVat(num float64, vat float64) float64 {
+	return num * (1 + (vat / 100))
+}
 
 // ValidateZoneNumber takes an int as parameter and checks if it between 0 and 9
 // returns with error if not

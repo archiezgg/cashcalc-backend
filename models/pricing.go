747c4472258babc92c3514e7e8f26e032efc7a16
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-04-19 18:49:24 +0200

    refactor(json tags): add json tags to all models

--- models/pricing.go
+++ models/pricing.go
@@ -2,19 +2,19 @@ package models
 
 // Pricing is the struct to store zone numbers and the matching fares
 type Pricing struct {
-	ZoneNumber int   `bson:"zoneNumber"`
-	Fares      []Fare `bson:"fares"`
-	DocFares   []Fare `bson:"docFares"`
+	ZoneNumber int    `bson:"zoneNumber" json:"zoneNumber"`
+	Fares      []Fare `bson:"fares" json:"fares"`
+	DocFares   []Fare `bson:"docFares" json:"docFares"`
 }
 
 //Pricings stores both air and road pricings as fields
 type Pricings struct {
-	AirPricings  []Pricing `bson:"airPricings"`
-	RoadPricings []Pricing `bson:"roadPricings"`
+	AirPricings  []Pricing `bson:"airPricings" json:"airPricings"`
+	RoadPricings []Pricing `bson:"roadPricings" json:"roadPricings"`
 }
 
 // Fare stores the weight and the corresponding base fare of the package
 type Fare struct {
-	Weight   float64
-	BaseFare int
+	Weight   float64 `bson:"weight" json:"weight"`
+	BaseFare int     `bson:"baseFare" json:"baseFare"`
 }
--- models/pricing_variables.go
+++ models/pricing_variables.go
@@ -2,16 +2,16 @@ package models
 
 //PricingVariables is the struct to store the variables that can be set by the admin of the application
 type PricingVariables struct {
-	VATPercent          float64 `bson:"vatPercent"`
-	AirFuelFarePercent  float64 `bson:"airFuelFarePercent"`
-	RoadFuelFarePercent float64 `bson:"roadFuelFarePercent"`
-	Express9h           int     `bson:"express9h"`
-	Express9hHungarian  int     `bson:"express9hHun"`
-	Express12h          int     `bson:"express12h"`
-	Express12hHungarian int     `bson:"express12hHun"`
-	InsuranceLimit      int     `bson:"insuranceLimit"`
-	MinInsurance        int     `bson:"minInsurance"`
-	EXT                 int     `bson:"ext"`
-	RAS                 int     `bson:"ras"`
-	TK                  int     `bson:"tk"`
+	VATPercent          float64 `bson:"vatPercent" json:"vatPercent"`
+	AirFuelFarePercent  float64 `bson:"airFuelFarePercent" json:"airFuelFarePercent"`
+	RoadFuelFarePercent float64 `bson:"roadFuelFarePercent" json:"roadFuelFarePercent"`
+	Express9h           int     `bson:"express9h" json:"express9h"`
+	Express9hHungarian  int     `bson:"express9hHun" json:"express9hHun"`
+	Express12h          int     `bson:"express12h" json:"express12h"`
+	Express12hHungarian int     `bson:"express12hHun" json:"express12hHun"`
+	InsuranceLimit      int     `bson:"insuranceLimit" json:"insuranceLimit"`
+	MinInsurance        int     `bson:"minInsurance" json:"minInsurance"`
+	EXT                 int     `bson:"ext" json:"ext"`
+	RAS                 int     `bson:"ras" json:"ras"`
+	TK                  int     `bson:"tk" json:"tk"`
 }

693b04f1f91cb028a7377396b80b49086fbca366
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-04-20 21:09:47 +0200

    refactor(country handler): refactor countries endpoints

--- controllers/country_controller.go
+++ controllers/country_controller.go
@@ -2,38 +2,47 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/IstvanN/cashcalc-backend/repositories"
+	"github.com/IstvanN/cashcalc-backend/services"
 	"github.com/gorilla/mux"
 )
 
 var countriesEndpoint = os.Getenv("COUNTRIES_ENDPOINT")
 
 func registerCountriesRoutes(router *mux.Router) {
-	router.HandleFunc(countriesEndpoint, allCountriesHandler).
-		Methods("GET").
-		Queries("type", "{type:[a-zA-Z]+}")
+	router.HandleFunc(countriesEndpoint, allCountriesHandler).Methods(http.MethodGet)
+	router.HandleFunc(countriesEndpoint+"/air", airCountriesHandler).Methods(http.MethodGet)
+	router.HandleFunc(countriesEndpoint+"/road", roadCountriesHandler).Methods(http.MethodGet)
 }
 
 func allCountriesHandler(w http.ResponseWriter, r *http.Request) {
 	setContentTypeToJSON(w)
-	switch t := mux.Vars(r)["type"]; t {
-	case "air":
-		countriesAir, err := repositories.GetCountriesAirFromDB()
-		if err != nil {
-			log.Println(err)
-		}
-		json.NewEncoder(w).Encode(countriesAir)
-	case "road":
-		countriesRoad, err := repositories.GetCountriesRoadFromDB()
-		if err != nil {
-			log.Println(err)
-		}
-		json.NewEncoder(w).Encode(countriesRoad)
-	default:
-		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+	c, err := services.GetCountries()
+	if err != nil {
+		logErrorAndSendHTTPError(w, err, 500)
+		return
 	}
+	json.NewEncoder(w).Encode(c)
+}
+
+func airCountriesHandler(w http.ResponseWriter, r *http.Request) {
+	setContentTypeToJSON(w)
+	ac, err := services.GetAirCountries()
+	if err != nil {
+		logErrorAndSendHTTPError(w, err, 500)
+		return
+	}
+	json.NewEncoder(w).Encode(ac)
+}
+
+func roadCountriesHandler(w http.ResponseWriter, r *http.Request) {
+	setContentTypeToJSON(w)
+	rc, err := services.GetRoadCountries()
+	if err != nil {
+		logErrorAndSendHTTPError(w, err, 500)
+		return
+	}
+	json.NewEncoder(w).Encode(rc)
 }

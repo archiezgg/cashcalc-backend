a914ea3e7f48b721520a4496bb93a997b3aacd1c
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-04-20 21:05:08 +0200

    refactor(country service): init country service and copy funcs county repository

--- repositories/country_repository.go
+++ repositories/country_repository.go
@@ -12,27 +12,8 @@ var (
 	countriesCollectionName = os.Getenv("COUNTRIES_COLL")
 )
 
-// GetCountriesAirFromDB returns with a slice of all air elements of the Countries collection, or an error
-func GetCountriesAirFromDB() ([]models.Country, error) {
-	c, err := getCountriesFromDB()
-	if err != nil {
-		return nil, err
-	}
-
-	return c.CountriesAir, nil
-}
-
-// GetCountriesRoadFromDB returns with an array of all road elements of the Countries collection, or an error
-func GetCountriesRoadFromDB() ([]models.Country, error) {
-	c, err := getCountriesFromDB()
-	if err != nil {
-		return nil, err
-	}
-
-	return c.CountriesRoad, nil
-}
-
-func getCountriesFromDB() (models.Countries, error) {
+// GetCountriesFromDB queries all countries from db
+func GetCountriesFromDB() (models.Countries, error) {
 	coll := database.GetCollectionByName(countriesCollectionName)
 
 	var c models.Countries

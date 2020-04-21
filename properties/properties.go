d41829eb71af63f20d5d3714b9e9bd84252b689a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-04-21 21:33:44 +0200

    feat(properties): add min and max zn and weight

--- properties/properties.go
+++ properties/properties.go
@@ -22,7 +22,19 @@ const (
 	// PricingsEndpoint is the endpoint for pricings
 	PricingsEndpoint = "endpoint.pricings"
 	// CountriesEndpoint is the endpoint for countries
-	CountriesEndpoint = "endpoint.countries"
+	CountriesEndpoint    = "endpoint.countries"
+	AirFaresZnMin        = "air.fares.zn.min"
+	AirFaresZnMax        = "air.fares.zn.max"
+	AirDocFaresZnMin     = "air.docfares.zn.min"
+	AirDocFaresZnMax     = "air.docfares.zn.max"
+	RoadFaresZnMin       = "road.fares.zn.min"
+	RoadFaresZnMax       = "road.fares.zn.max"
+	AirFaresWeightMin    = "air.fares.weight.min"
+	AirFaresWeightMax    = "air.fares.weight.max"
+	AirDocFaresWeightMin = "air.docfares.weight.min"
+	AirDocFaresWeightMax = "air.docfares.weight.max"
+	RoadFaresWeightMin   = "road.fares.weight.min"
+	RoadFaresWeightMax   = "road.fares.weight.max"
 )
 
 // This function gets executed automatically when the app initializes

e3e60a87e1df0021471440d1caef15aeef3cadea
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-04-23 12:46:52 +0200

    refactor(properties): rename property consts and introduce exported variables

--- properties/properties.go
+++ properties/properties.go
@@ -5,37 +5,68 @@ import (
 )
 
 var (
-	// Prop is the proprties that gets loaded when the app inits
-	Prop           *properties.Properties
 	propertiesFile = "app.properties"
 )
 
 const (
+	pricingsCollectionProp    = "collection.pricings"
+	countriesCollectionProp   = "collection.countries"
+	pricingVarsCollectionProp = "collection.pricingvars"
+	pricingsEndpointProp      = "endpoint.pricings"
+	countriesEndpointProp     = "endpoint.countries"
+	airFaresZnMinProp         = "air.fares.zn.min"
+	airFaresZnMaxProp         = "air.fares.zn.max"
+	airDocFaresZnMinProp      = "air.docfares.zn.min"
+	airDocFaresZnMaxProp      = "air.docfares.zn.max"
+	roadFaresZnMinProp        = "road.fares.zn.min"
+	roadFaresZnMaxProp        = "road.fares.zn.max"
+	airFaresWeightMinProp     = "air.fares.weight.min"
+	airFaresWeightMaxProp     = "air.fares.weight.max"
+	airDocFaresWeightMinProp  = "air.docfares.weight.min"
+	airDocFaresWeightMaxProp  = "air.docfares.weight.max"
+	roadFaresWeightMinProp    = "road.fares.weight.min"
+	roadFaresWeightMaxProp    = "road.fares.weight.max"
+)
+
+var (
 	// PricingsCollection is the name of the DB collection of pricings
-	PricingsCollection = "collection.pricings"
+	PricingsCollection string
 	// CountriesCollection is the name of the DB collection of countries
-	CountriesCollection = "collection.countries"
+	CountriesCollection string
 	// PricingVarsCollection is the name of the DB collection of pricing variables
-	PricingVarsCollection = "collection.pricingvars"
+	PricingVarsCollection string
 	// PricingsEndpoint is the endpoint for pricings
-	PricingsEndpoint = "endpoint.pricings"
+	PricingsEndpoint string
 	// CountriesEndpoint is the endpoint for countries
-	CountriesEndpoint    = "endpoint.countries"
-	AirFaresZnMin        = "air.fares.zn.min"
-	AirFaresZnMax        = "air.fares.zn.max"
-	AirDocFaresZnMin     = "air.docfares.zn.min"
-	AirDocFaresZnMax     = "air.docfares.zn.max"
-	RoadFaresZnMin       = "road.fares.zn.min"
-	RoadFaresZnMax       = "road.fares.zn.max"
-	AirFaresWeightMin    = "air.fares.weight.min"
-	AirFaresWeightMax    = "air.fares.weight.max"
-	AirDocFaresWeightMin = "air.docfares.weight.min"
-	AirDocFaresWeightMax = "air.docfares.weight.max"
-	RoadFaresWeightMin   = "road.fares.weight.min"
-	RoadFaresWeightMax   = "road.fares.weight.max"
+	CountriesEndpoint string
+	// AirFaresZnMin is the minimum zone number for air fares
+	AirFaresZnMin int
+	// AirFaresZnMax is the maximum zone number for air fares
+	AirFaresZnMax int
+	// AirDocFaresZnMin is the minimum zone number for document fares
+	AirDocFaresZnMin int
+	// AirDocFaresZnMax is the maximum zone number for document fares
+	AirDocFaresZnMax int
+	// RoadFaresZnMin is the minimum zone number for road fares
+	RoadFaresZnMin int
+	// RoadFaresZnMax is the maxmimum zone number for road fares
+	RoadFaresZnMax int
+	// AirFaresWeightMin is the minimum weight for air fares
+	AirFaresWeightMin int
+	// AirFaresWeightMax is the maxmimum weight for air fares
+	AirFaresWeightMax float64
+	// AirDocFaresWeightMin is the minimum weight for air document fares
+	AirDocFaresWeightMin float64
+	// AirDocFaresWeightMax is the maxmimum weight for air document fares
+	AirDocFaresWeightMax float64
+	// RoadFaresWeightMin is the minimmum weight for road fares
+	RoadFaresWeightMin float64
+	// RoadFaresWeightMax is the maxmimum weight for road fares
+	RoadFaresWeightMax float64
 )
 
 // This function gets executed automatically when the app initializes
 func init() {
-	Prop = properties.MustLoadFile(propertiesFile, properties.UTF8)
+	p := properties.MustLoadFile(propertiesFile, properties.UTF8)
+	PricingsCollection = p.GetString(pricingsCollectionProp, "/pricings")
 }

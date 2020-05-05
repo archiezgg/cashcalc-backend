685000dd5113805a466ca3f3d7ed65809b30946d
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-05 22:38:59 +0200

    feat(pricing_vars): add repository and base get func for pricing vars

---
+++ repositories/pricing_variable_repository.go
@@ -0,0 +1,24 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/IstvanN/cashcalc-backend/database"
+	"github.com/IstvanN/cashcalc-backend/models"
+	"github.com/IstvanN/cashcalc-backend/properties"
+)
+
+// GetPricingVariables queries the db for
+// the pricing variables that can be set by the admin
+func GetPricingVariables() (models.PricingVariables, error) {
+	coll := database.GetCollectionByName(properties.PricingVarsCollection)
+
+	var pv models.PricingVariables
+	err := coll.Find(nil).One(&pv)
+	if err != nil {
+		return models.PricingVariables{}, fmt.Errorf("error while retreiving collection %v from db: %v",
+			properties.PricingVarsCollection, err)
+	}
+
+	return pv, nil
+}

32de9c3469b73f3fb244baa830a355310313eb96
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-07 02:08:53 +0200

    feat(login): implement login endpoint in properties

--- controllers/login_controller.go
+++ controllers/login_controller.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/IstvanN/cashcalc-backend/properties"
+
 	"github.com/IstvanN/cashcalc-backend/security"
 
 	"github.com/gorilla/mux"
 )
 
 func registerLoginRoutes(router *mux.Router) {
-	router.HandleFunc("/login", loginHandler).Methods(http.MethodGet)
+	ep := properties.LoginEndpoint
+	router.HandleFunc(ep, loginHandler).Methods(http.MethodGet)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {

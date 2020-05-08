9578cf5d92fa8e3ade57e883b7fb90f1c23f4b3b
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-08 22:15:28 +0200

    feat(user): create repository with DB functionalities for user

---
+++ repositories/user_repository.go
@@ -0,0 +1,37 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/IstvanN/cashcalc-backend/database"
+	"github.com/IstvanN/cashcalc-backend/models"
+)
+
+//GetUsers retrieves all users from the database
+func GetUsers() (models.Users, error) {
+	coll := database.GetCollectionByName("users")
+
+	var u models.Users
+	err := coll.Find(nil).One(&u)
+	if err != nil {
+		return models.Users{}, fmt.Errorf("error while retrieving collection %v from database: %v",
+			"users", err)
+	}
+
+	return u, nil
+}
+
+// GetUserByRole retrieves the user by its role
+func GetUserByRole(role models.Role) (models.User, error) {
+	users, err := GetUsers()
+	if err != nil {
+		return models.User{}, err
+	}
+
+	for _, u := range users.Users {
+		if u.Role == role {
+			return u, nil
+		}
+	}
+	return models.User{}, nil
+}

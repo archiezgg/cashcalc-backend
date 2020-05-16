6cd221d53b9083a6edf395611d9f515e3d166343
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-16 11:21:53 +0200

    feat(repo): add get and save funcs for token repo

--- repositories/token_repository.go
+++ repositories/token_repository.go
@@ -12,7 +12,17 @@ import (
 	"github.com/IstvanN/cashcalc-backend/properties"
 )
 
+// GetRoleFromRefreshToken retrieves the role to the corresponding refresh token
+func GetRoleFromRefreshToken(rt string) (models.Role, error) {
+	role, err := database.RedisClient().Get(rt).Result()
+	if err != nil {
+		return "", err
+	}
+	return models.Role(role), nil
+}
+
 // SaveRefreshToken saves the token with the role to the DB
-func SaveRefreshToken(rt string, role models.Role) {
-	database.RedisClient().Set(rt, role, properties.RefreshTokenExp)
+func SaveRefreshToken(rt string, role models.Role) error {
+	err := database.RedisClient().Set(rt, role, properties.RefreshTokenExp).Err()
+	return err
 }

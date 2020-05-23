aa04b0a2fb2188d4066a95997006463fc2c666c0
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-23 10:03:26 +0200

    feat(calc_service): add function to decide if zone is eu

--- services/calc_service.go
+++ services/calc_service.go
@@ -10,14 +10,22 @@ import (
 	"github.com/IstvanN/cashcalc-backend/models"
 )
 
+// CalcResultAir takes input data and calculates the fares for air AND hungarian delivery
 func CalcResultAir(input models.CalcInputData) (models.CalcOutputData, error) {
 	return models.CalcOutputData{}, nil
 }
 
 func isZoneEU(zn int) bool {
-	return zn < 5
+	return zn <= 4 && zn >= 0
 }
 
-func calcBaseFareWithDiscountAir(zn int, vat float64, baseFare models.Fare) float64 {
-	return 0.0
+func calcBaseFareWithDiscountAir(zn int, vat float64, pricingFare models.Fare) float64 {
+	var result float64
+	if isZoneEU(zn) {
+		result = addDiscountToBaseFare(float64(pricingFare.BaseFare) * vat)
+	}
+}
+
+func addDiscountToBaseFare(discountPercent int, baseFare float64) float64 {
+	return float64(1-discountPercent/100) * baseFare
 }

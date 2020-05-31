f186cff797865bb377f2cbb3bc1020fadd23391b
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-05-31 12:09:02 +0200

    feat(test): add test for CalcBaseFareWithVatAndDiscountAir

--- services/calc_service_test.go
+++ services/calc_service_test.go
@@ -35,12 +35,36 @@ func TestApplyDiscountToBaseFare(t *testing.T) {
 		expected      float64
 	}{
 		{100, 10, 90},
+		{200, 10, 180},
+		{100, 90, 10},
 	}
 
 	for _, tc := range testCases {
 		actual := applyDiscountToBaseFare(tc.inputBaseFare, tc.inputDiscount)
 		if tc.expected != actual {
 			t.Errorf("applyDiscountToBaseFare(%v, %v) failed: expected %v, got %v", tc.inputBaseFare, tc.inputDiscount, tc.expected, actual)
+		}
+	}
+}
+
+func TestCalcBaseFareWithVatAndDiscountAir(t *testing.T) {
+	testCases := []struct {
+		zoneNumber                  int
+		discountPercent, vatPercent float64
+		baseFare                    int
+		expected                    float64
+	}{
+		{0, 20, 27, 3371, 3425},
+		{1, 10, 27, 14992, 17136},
+		{5, 30, 27, 57184, 40029},
+		{6, 10, 27, 21093, 18984},
+	}
+
+	for _, tc := range testCases {
+		actual := CalcBaseFareWithVatAndDiscountAir(tc.zoneNumber, tc.discountPercent, tc.vatPercent, tc.baseFare)
+		if tc.expected != actual {
+			t.Errorf("CalcBaseFareWithVatAndDiscountAir(%v, %v, %v, %v) failed: expected %v, got %v", tc.zoneNumber, tc.discountPercent, tc.vatPercent, tc.baseFare,
+				tc.expected, actual)
 		}
 	}
 }

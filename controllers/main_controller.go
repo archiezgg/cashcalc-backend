82ec2aac20cdb35a9a15e1d7a56015616fa5a3b4
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-07-11 11:37:23 +0200

    fix(CORS): add returning if method OPTIONS in header middleware

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -32,13 +32,18 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	writeMessage(w, "Welcome to CashCalc!")
 }
 
-// setHeaderMiddleWare sets the header with some pre-made options
+// setHeaderMiddleWare sets the header with some pre-made CORS-enabling options
 func setHeaderMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-PINGOTHER")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }

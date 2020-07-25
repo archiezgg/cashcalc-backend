a6c26311d2831e190b44e5b3252aaf2b57b9872c
Istvan Nemeth dr <40436732+IstvanN@users.noreply.github.com>
2020-07-25 20:20:12 +0200

    fix(cors): add allow origin to localhost

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -36,7 +36,7 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 func setHeaderMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-PINGOTHER")
 

8d8ac693296d56618a5e46f155654016f01febba
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-08-02 10:03:16 +0200

    feat(environment): add ENVIRONMENT env var and set headers whether it's DEV or PROD

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -9,6 +9,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -36,11 +37,15 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 func setHeaderMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		// w.Header().Set("Access-Control-Allow-Origin", "https://cashcalc.web.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-PINGOTHER")
+
+		if os.Getenv("ENVIRONMENT") == "DEV" {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "https://cashcalc.web.app")
+		}
 
 		if r.Method == "OPTIONS" {
 			return

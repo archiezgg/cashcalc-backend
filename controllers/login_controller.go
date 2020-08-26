99af395cb08fdfa42c26708472433efd466ffbb1
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-08-26 22:57:01 +0200

    feat(is-authorized): add endpoint to properties

--- controllers/login_controller.go
+++ controllers/login_controller.go
@@ -24,7 +24,7 @@ func registerLoginRoutes(router *mux.Router) {
 	router.HandleFunc(properties.LoginEndpoint, loginHandler).Methods(http.MethodPost, http.MethodOptions)
 	router.HandleFunc(properties.RefreshEndpoint, refreshHandler).Methods(http.MethodPost, http.MethodOptions)
 	router.HandleFunc(properties.LogoutEndpoint, logoutHandler).Methods(http.MethodPost, http.MethodOptions)
-	router.HandleFunc("/is-authorized", isAuthorizedHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc(properties.IsAuthorizedEndpoint, isAuthorizedHandler).Methods(http.MethodGet, http.MethodOptions)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {

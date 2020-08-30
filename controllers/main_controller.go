7f4bc60c9d68e17389e7d4aefe44abbd3c3baf4d
Istvan Nemeth dr <40436732+IstvanN@users.noreply.github.com>
2020-08-31 00:46:57 +0200

    fix(cors): add patch to allowed methods

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -44,7 +44,7 @@ func setHeaderMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-PINGOTHER")
 

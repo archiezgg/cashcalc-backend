7d850519bf54d5fc468205661f3fd5b774f33093
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-08-30 16:46:14 +0200

    feat(postgres): add func to get the DB specs from URL

--- services/database_service.go
+++ services/database_service.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-// GetMongoDBNameFromURI splits the given URI and returns the name of the DB
+// GetMongoDBNameFromURI splits the given mongo URI and returns the name of the DB
 func GetMongoDBNameFromURI(uri string) string {
 	splitURI := strings.SplitAfter(uri, "/")
 	dbName := splitURI[len(splitURI)-1]
@@ -20,4 +20,17 @@ func GetMongoDBNameFromURI(uri string) string {
 	}
 
 	return dbName
+}
+
+// GetPostgresDBSpecsFromURL splits the given postgres URL and returns with the specs separated
+func GetPostgresDBSpecsFromURL(dbURL string) (dbHost, dbUser, dbPort, dbPw, dbName string) {
+	userAndPw := strings.Replace(strings.Split(dbURL, "@")[0], "postgres://", "", -1)
+	hostAndPortAndName := strings.Split(dbURL, "@")[1]
+
+	dbUser = strings.Split(userAndPw, ":")[0]
+	dbPw = strings.Split(userAndPw, ":")[1]
+	dbHost = strings.Split(hostAndPortAndName, ":")[0]
+	dbPort = strings.Split(strings.Split(hostAndPortAndName, ":")[1], "/")[0]
+	dbName = strings.Split(strings.Split(hostAndPortAndName, ":")[1], "/")[1]
+	return
 }

48103d47e40442fa7f1b66d36e93a2bb401ac6fd
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-09-02 22:11:54 +0200

    feat(refresh_token): add func to delete all refresh tokens for user

--- repositories/refresh_token_repository.go
+++ repositories/refresh_token_repository.go
@@ -28,6 +28,7 @@ func CreateRefreshTokenAndSaveForUser(user models.User, tokenString string, expi
 	return rt, nil
 }
 
+// GetRefreshTokenByTokenString retrieves the rt by ID (token string)
 func GetRefreshTokenByTokenString(tokenString string) (models.RefreshToken, error) {
 	var rt models.RefreshToken
 	result := database.GetPostgresDB().First(&rt)
@@ -47,6 +48,7 @@ func GetRefreshTokensByUserID(userID uint) ([]models.RefreshToken, error) {
 	return refreshTokens, nil
 }
 
+// GetAllRefreshTokens retrieves all refresh tokens stored in DB
 func GetAllRefreshTokens() ([]models.RefreshToken, error) {
 	var refreshTokens []models.RefreshToken
 	result := database.GetPostgresDB().Find(&refreshTokens)
@@ -56,6 +58,7 @@ func GetAllRefreshTokens() ([]models.RefreshToken, error) {
 	return refreshTokens, nil
 }
 
+// DeleteRefreshTokenByTokenString deletes the refresh token by ID (token string)
 func DeleteRefreshTokenByTokenString(tokenString string) error {
 	rt, err := GetRefreshTokenByTokenString(tokenString)
 	if err != nil {
@@ -63,6 +66,15 @@ func DeleteRefreshTokenByTokenString(tokenString string) error {
 	}
 
 	result := database.GetPostgresDB().Delete(rt)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+// DeleteAllRefreshTokensForUser deletes all refresh tokens for given user
+func DeleteAllRefreshTokensForUser(userID uint) error {
+	result := database.GetPostgresDB().Where("user_id = ?", userID).Delete(&models.RefreshToken{})
 	if result.Error != nil {
 		return result.Error
 	}

0424ec0b56b1275e5e4f0a90729665a153622d02
Istvan Nemeth <nemethistvan.ius@gmail.com>
2020-09-07 19:13:55 +0200

    feat(test): write unit test for GetPostgresSpecs func

--- services/database_service_test.go
+++ services/database_service_test.go
@@ -30,3 +30,21 @@ func TestGetMongoDBNameFromURI(t *testing.T) {
 		}
 	}
 }
+
+func TestGetPostgresDBSpecsFromURL(t *testing.T) {
+	testCases := []struct {
+		input, expectedUser, expectedPw, expectedHost, expectedPort, expectedDBName string
+	}{
+		{"protocol://user:pw@host:port/db", "user", "pw", "host", "port", "db"},
+	}
+
+	for _, tc := range testCases {
+		actualHost, actualUser, actualPort, actualPw, actualDB := GetPostgresDBSpecsFromURL(tc.input)
+		if actualHost != tc.expectedHost && actualUser != tc.expectedUser &&
+			actualPort != tc.expectedPort && actualPw != tc.expectedPw && actualDB != tc.expectedDBName {
+			t.Errorf("GetPostgresDBSpecsFromURL(%v) failed: exptected: protocol://%v:%v@%v:%v/%v, got: protocol://%v:%v@%v:%v/%v",
+				tc.input, tc.expectedUser, tc.expectedPw, tc.expectedHost, tc.expectedPort, tc.expectedDBName,
+				actualUser, actualPw, actualHost, actualPort, actualDB)
+		}
+	}
+}
